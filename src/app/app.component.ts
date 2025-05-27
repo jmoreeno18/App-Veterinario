9945b35d65b9de45556ff6f99df46c87159179bf
MateoManuel <mmf0048@alu.medac.es>
2025-05-27 17:04:21 +0200

    Añadido formulario de login. Añadido componente para el restablecimiento de la contraseña, y formulario para dicha acción. Añadida ruta reset-password que pertenece al formulario de restablecimiento de contraseña. Añadido logo en las vistas de login y modificación de contraseña.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SupabaseService } from './supabase/supabase.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mockvet';
+
+  constructor(public supabaseService: SupabaseService) {}
+
+  async ngOnInit() {
+    await this.supabaseService.restoreSession();
+  }
 }
